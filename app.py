d1f2a657120d0f65600a5083353d5cea259e5439
Ritik Kumar Chhitrolia <ritikkumarchhitrolia@gmail.com>
2026-05-22 13:09:33 +0530

    Updated files for huggingface deployement setup

--- app.py
+++ app.py
@@ -147,8 +147,10 @@ def send_image(filename):
 def send_example(filename):
     return send_from_directory('examples', filename)
 
+# if __name__ == '__main__':
+#     from werkzeug.serving import run_simple
+#     run_simple('localhost', 5000, app, use_reloader=True, use_debugger=True)
+
+
 if __name__ == '__main__':
-    from werkzeug.serving import run_simple
-    run_simple('localhost', 5000, app, use_reloader=True, use_debugger=True)
-
-
+    app.run(host='0.0.0.0', port=7860)
